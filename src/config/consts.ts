66d91233d84b1b3551bdebec3cf9dbfb12add813
Gabriel Soares <g4brieloliveirasoares@gmail.com>
2026-03-16 15:27:12 -0300

    .

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Astro Boy';
-export const SITE_DESCRIPTION = 'Um site feito para mostrar o poder do framework Astro!';
+export const SITE_TITLE = 'Astro + Notion';
+export const SITE_DESCRIPTION = 'Um site feito com Notion como CMS para extrair o máximo que o framework Astro pode oferecer em blogs!';
 export const SITE_IMAGE = '/front-page.png';
