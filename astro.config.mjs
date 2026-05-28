95ac703ba01677dff4a7ae2cbd5d807a6283df99
Gabriel Soares <g4brieloliveirasoares@gmail.com>
2026-05-27 22:57:21 -0300

    testes / node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: process.env.SITE_URL || "http://localhost:4321",
   integrations: [sitemap()],
   output: "static",
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone',
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
